ef6e2d08f85cc911b6ceba93fdb59ed8ef861b93
anmichalak <wievowu@wp.pl>
2018-08-25 17:27:36 +0200

    My Django Girls app, first commit

---
+++ cwiczenia/python_intro.py
@@ -0,0 +1,9 @@
+def hi(imie):
+    print('Hej ' +imie+ '!')
+
+dziewczyny = ["Ania", "Iza", "Kasia", "Phoebe"]
+for imie in dziewczyny:
+    hi(imie)
+    print('Kolejna dziewczyna')
+for i in range(1,8):
+    print(i)
